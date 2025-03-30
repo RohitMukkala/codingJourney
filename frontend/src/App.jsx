7ea73880acd5e4d77215087f895da93cb7b547ec
Rohit Mukkala <mukkalarohit@gmail.com>
2025-03-30 23:51:55 +0530

    till routing dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button } from "@/components/ui/button";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-green-600">
-      Se <Button> </Button>
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 }
 
